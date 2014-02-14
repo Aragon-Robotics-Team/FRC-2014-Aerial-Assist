d73b15016348181d6cd26769a005c63567d4fe5b
aragonrobotics <aragon.robotics.team@gmail.com>
2014-02-14 01:57:43 +0000

    Calibration.cpp edited online with Bitbucket

--- Calibration.cpp
+++ Calibration.cpp
@@ -1,60 +1,95 @@
 #include "WPILib.h"
-#include "Gamepad.h"
-#include "Compressor.h"
 
 class RobotDemo : public SimpleRobot
 {
-	Talon motor0, motor1, motor2, motor3;
-	Compressor compressThing;
-	Gamepad padThing;
-	Timer timeThing;
+	Talon motor1, motor2, motor3, motor4, motor5, motor6, motor7, motor8, motor9, motor10;
+	DriverStationLCD *driverStation;
+
 
 
 public:
 	RobotDemo():
-		motor0(0),
 		motor1(1),
 		motor2(2),
 		motor3(3),
-		compressThing(4, 5),
-		padThing(6),
-		timeThing()
+		motor4(4),
+		motor5(5),
+		motor6(6),
+		motor7(7),
+		motor8(8),
+		motor9(9),
+		motor10(10)
 	{
 		
 	}
 
 	void Autonomous()
 	{
-	
+		driverStation = DriverStationLCD::GetInstance();
+		
+		while(true){
+			motor1.Set(0);
+			motor2.Set(0);
+			motor3.Set(0);
+			motor4.Set(0);
+			motor5.Set(0);
+			motor6.Set(0);
+			motor7.Set(0);
+			motor8.Set(0);
+			motor9.Set(0);
+			motor10.Set(0);
+			driverStation->PrintfLine(DriverStationLCD::kUser_Line1, "NEUTRAL");
+			driverStation->UpdateLCD();
+			Wait(5);
+			
+			for(int i = 0; i < 5; i++){
+				motor1.Set(1);
+				motor2.Set(1);
+				motor3.Set(1);
+				motor4.Set(1);
+				motor5.Set(1);
+				motor6.Set(1);
+				motor7.Set(1);
+				motor8.Set(1);
+				motor9.Set(1);
+				motor10.Set(1);
+				driverStation->PrintfLine(DriverStationLCD::kUser_Line1, "FORWARD PULSE %d", i);
+				driverStation->UpdateLCD();
+				Wait(1);
+				
+				motor1.Set(-1);
+				motor2.Set(-1);
+				motor3.Set(-1);
+				motor4.Set(-1);
+				motor5.Set(-1);
+				motor6.Set(-1);
+				motor7.Set(-1);
+				motor8.Set(-1);
+				motor9.Set(-1);
+				motor10.Set(-1);
+				driverStation->PrintfLine(DriverStationLCD::kUser_Line1, "BACKWARD PULSE %d", i);
+				driverStation->UpdateLCD();
+				Wait(1);
+			}
+			motor1.Set(0);
+			motor2.Set(0);
+			motor3.Set(0);
+			motor4.Set(0);
+			motor5.Set(0);
+			motor6.Set(0);
+			motor7.Set(0);
+			motor8.Set(0);
+			motor9.Set(0);
+			motor10.Set(0);
+			driverStation->PrintfLine(DriverStationLCD::kUser_Line1, "NEUTRAL PULSE");
+			driverStation->UpdateLCD();
+			Wait(1);
+			
+		}
 	}
 	void OperatorControl()
 	{
-		while(padThing.getButton(4)){
-					motor0.Set(1);
-					motor1.Set(1);
-					motor2.Set(1);
-					motor3.Set(1);
-					Wait(1);
-					motor0.Set(-1);
-					motor1.Set(-1);
-					motor2.Set(-1);
-					motor3.Set(-1);
-					Wait(1);
-				}
-		while(padThing.getButton(1)){
-			motor0.Set(1);
-			motor1.Set(1);
-			motor2.Set(1);
-			motor3.Set(1);
-		}
-		while(padThing.getButton(2)){
-			motor0.Set(-1);
-			motor1.Set(-1);
-			motor2.Set(-1);
-			motor3.Set(-1);
-		}
-		
-		while(padThing.getButton(3)){
+	
 	}
 	
 	/**
